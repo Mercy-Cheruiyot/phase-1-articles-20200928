4f26acd8300d58aa820212e725fc93b6a9a9ad7d
Mercy <mercycheruiyot2013@gmail.com>
2022-10-30 23:19:48 +0300

    DONE

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,13 +1,24 @@
 class Author
   attr_reader :name
+  @@all=[]
 
 
   def initialize(name)
     @name = name
+    @@all << self
   
   end
   def name
     @name
+  end
+  def self.all
+    @all
+  end
+  def Articles
+    Article.all.select{|article|article.author==self}
 
-
+  end
+  def Magazines
+    Article.map{|article|article.magazine.name}.uniq
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,5 +1,6 @@
 class Magazine
   attr_accessor :name, :category
+  @@all=[]
 
 
   def initialize(name, category)
@@ -7,6 +8,28 @@ class Magazine
     @category = category
 
   end
+  def self.all
+    @@all
+  end
+  def Articles
+    Article.all.select{|article|article.magazine=self}
+
+  end
+  def article_titles
+
+  end
+
+  end
+  def Contributors
+    Articles.map{|article|article.author}
+  end
+  def find_by_name
+
+  end
+  def contributing_authors
+    @@all.select{|article|article.contributors>2}
+  end
+
 
 
 end
